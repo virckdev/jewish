505cc522c26bcbb5cdec2e4662d3b6cd267716b5
virckdev <wellwellwellboyeatingchicken@gmail.com>
2026-05-17 19:14:33 -0500

    Set SCRIPT_KEY for krampus.lua

---
+++ krampus.lua
@@ -0,0 +1,3 @@
+getgenv().SCRIPT_KEY = "KRP-d6986a62-d4fc-4570-897a-354926f77d21";
+
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/00b9c0e8826aa44baa6a7976a6b56762fd94c467410d7a68afa58b3bb3e4f5d7/download"))()
