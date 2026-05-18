e64c074257d1e4a424401e9a56ca75c1975902bf
virckdev <wellwellwellboyeatingchicken@gmail.com>
2026-05-17 20:53:54 -0500

    Refactor accept.lua to remove key and add warning
    Removed SCRIPT_KEY assignment and added a success warning.

--- accept.lua
+++ accept.lua
@@ -1,3 +1,1 @@
-getgenv().SCRIPT_KEY = "KRP-d6986a62-d4fc-4570-897a-354926f77d21";
-
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/cd6396944c444c64939f3592c325e88f81b41c50a26ea03fcbdb9641e13684c9/download"))()
+warn("loaded successfully")
